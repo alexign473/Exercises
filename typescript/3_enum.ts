2e464604f5333d024316d048c9741a677e8e350c
Alexander Ignatev <alexign473@gmail.com>
2022-08-17 18:35:47 +0300

    first commit

---
+++ typescript/3_enum.ts
@@ -0,0 +1,19 @@
+enum Membership {
+  Simple,
+  Standard,
+  Premium,
+}
+
+const membership = Membership.Standard;
+const membershipReverse = Membership[2];
+console.log(membership);
+console.log(membershipReverse);
+
+enum SocialMedia {
+  VK = 'VK',
+  FACEBOOK = 'FACEBOOK',
+  INSTA = 'INSTA',
+}
+
+const social = SocialMedia.INSTA;
+console.log(social);
